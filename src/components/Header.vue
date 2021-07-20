3cae44692251af35069fe140a30bc616709558a4
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-07-19 21:10:15 -0500

    Primera prueba

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.Header
+    .navbar.px-5
+        .container-fluid
+            a.navbar-brand(href="")
+                | SAPOOO
+
+
+
+</template>
+
+<script>
+export default {
+    name: 'Header'
+    
+}
+</script>
+
+<style lang="sass" scoped>
+.navbar 
+    background-color: gray
+    
+</style>
